bb651034bbff22035a87ccf2eb5d2327a3f2e057
Fatih ADAş <fatihadas93@gmail.com>
2019-03-13 13:17:33 +0300

    Add project files.

---
+++ JsonService/IService.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.ServiceModel;
+using System.ServiceModel.Web;
+using System.Web;
+
+namespace JsonService
+{
+
+    [ServiceContract]
+    public interface IService
+    {
+        [OperationContract]
+        void DoWork();
+
+        
+        [OperationContract]
+        [WebInvoke(Method ="POST",ResponseFormat =WebMessageFormat.Json,
+            BodyStyle =WebMessageBodyStyle.Wrapped,UriTemplate ="json/sayhi")]
+        string SayHi(string name);
+
+    }
+    
+}
---
+++ JsonService/Service.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace JsonService
+{
+    public class Service : IService
+    {
+        public void DoWork()
+        {
+
+        }
+
+        public string SayHi(string name)
+        {
+            return "Merhaba " + name;
+        }
+    }
+}
